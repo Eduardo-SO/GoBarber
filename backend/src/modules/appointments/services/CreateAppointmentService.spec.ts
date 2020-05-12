7204d65ddd0f1b4b41d1d09585b7497bdc5b62d9
Eduardo SO <eduardo.sool07@gmail.com>
2020-05-12 17:47:46 -0300

    test: Should not be able to create two appointments at the same time

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,19 +1,42 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
   it('Should be able to create a new appointment', async () => {
     const fakeAppointmentsRepository = new FakeAppointmentsRepository();
-    const createAppointmentService = new CreateAppointmentService(
+    const createAppointment = new CreateAppointmentService(
       fakeAppointmentsRepository,
     );
 
-    const appointment = await createAppointmentService.execute({
+    const appointment = await createAppointment.execute({
       date: new Date(),
       provider_id: '123456',
     });
 
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe('123456');
+  });
+
+  it('Should not be able to create two appoinments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date();
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123456',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
