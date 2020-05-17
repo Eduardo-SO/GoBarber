1dfbf92e8f4cdc2b0e3f574801e58f7a0df43a9b
Eduardo SO <eduardo.sool07@gmail.com>
2020-05-17 18:16:33 -0300

    feat: Show user profile

---
+++ backend/src/modules/users/services/ShowProfileService.spec.ts
@@ -0,0 +1,30 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import ShowProfileService from './ShowProfileService';
+
+let showProfile: ShowProfileService;
+let fakeUsersRepository: FakeUsersRepository;
+
+describe('SendForgotPasswordEmail', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+
+    showProfile = new ShowProfileService(fakeUsersRepository);
+  });
+
+  it('Should be able to show the profile', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123123',
+    });
+
+    await showProfile.execute({
+      user_id: user.id,
+    });
+
+    expect(user.name).toBe('John Doe');
+    expect(user.email).toBe('johndoe@example.com');
+  });
+});
