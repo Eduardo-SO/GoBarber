0a25b6d382e493d70a7b79b15790cb8cf707311e
Eduardo SO <eduardo.sool07@gmail.com>
2020-05-17 13:31:13 -0300

    feat: Send mail to Ethereal

---
+++ backend/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -0,0 +1,35 @@
+import nodemailer, { Transporter } from 'nodemailer';
+
+import IMailProvider from '../models/IMailProvider';
+
+export default class EtherealMailProvider implements IMailProvider {
+  private client: Transporter;
+
+  constructor() {
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+        user: 'USER',
+        pass: 'PASSWORD',
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    this.client = transporter;
+  }
+
+  public async sendMail(to: string, body: string): Promise<void> {
+    const message = await this.client.sendMail({
+      from: 'Equipe GoBarber <equipe@gobarber.com.br>',
+      to,
+      subject: 'Recuperação de senha',
+      text: body,
+    });
+
+    console.log('Message sent: %s', message.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+  }
+}
