5168fbd9066a2a0e878b5d9776cb51fc2af0b657
Eduardo SO <eduardo.sool07@gmail.com>
2020-05-20 18:50:14 -0300

    fix: Remove month test from day tests

--- backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -12,36 +12,6 @@ describe('ListProviderDayAvailability', () => {
     );
   });
 
-  it('Should be able to list the month availability from provider', async () => {
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      user_id: 'user',
-      date: new Date(2020, 4, 20, 8, 0, 0),
-    });
-
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      user_id: 'user',
-      date: new Date(2020, 4, 20, 10, 0, 0),
-    });
-
-    const availability = await listProviderDayAvailabilityService.execute({
-      provider_id: 'user',
-      day: 20,
-      month: 5,
-      year: 2020,
-    });
-
-    expect(availability).toEqual(
-      expect.arrayContaining([
-        { hour: 8, available: false },
-        { hour: 9, available: true },
-        { hour: 10, available: false },
-        { hour: 11, available: true },
-      ]),
-    );
-  });
-
   it('Should be able to list the day availability from provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
