9c59e3de9ed3f8a1665baea6d7791af843a91aaf
Eduardo SO <eduardo.sool07@gmail.com>
2020-05-20 23:58:13 -0300

    add: Route validations

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -3,6 +3,8 @@ import 'reflect-metadata';
 import express, { Response, Request, NextFunction } from 'express';
 import 'express-async-errors';
 
+import { errors } from 'celebrate';
+
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import routes from './routes';
@@ -19,6 +21,8 @@ app.listen(3333, () => {
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
+
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
