8e3e019a1883ab6cc1dc5d4d09c24f8f6aa82583
Eduardo SO <eduardo.sool07@gmail.com>
2020-05-23 20:19:25 -0300

    add: Global styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import GlobalStyles from './styles/global';
 
-const App: React.FC = () => {
-  return <div className="App" />;
-};
+const App: React.FC = () => (
+  <>
+    <h1>Hello dudes</h1>
+    <GlobalStyles />
+  </>
+);
 
 export default App;
