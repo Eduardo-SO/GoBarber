93427886c237a1f8107954aa10ff0161b233916c
Eduardo SO <eduardo.sool07@gmail.com>
2020-05-25 19:57:49 -0300

    add: cors

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -6,6 +6,7 @@ import 'express-async-errors';
 
 import { errors } from 'celebrate';
 
+import cors from 'cors';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import rateLimiter from './middlewares/rateLimiter';
@@ -20,6 +21,7 @@ app.listen(3333, () => {
   console.log('Server running on port 3333!');
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(rateLimiter);
 app.use('/files', express.static(uploadConfig.uploadsFolder));
