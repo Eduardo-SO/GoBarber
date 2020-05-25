68f29061492dbf26e81fb1eae9d3979b8ff09c86
Eduardo SO <eduardo.sool07@gmail.com>
2020-05-24 23:43:52 -0300

    add: Input focus style

--- frontend/src/components/Input/styles.ts
+++ frontend/src/components/Input/styles.ts
@@ -1,16 +1,27 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   display: flex;
   align-items: center;
 
   width: 100%;
-  border-radius: 10px;
   padding-left: 16px;
+  border-radius: 10px;
 
   border: 2px solid #232129;
-  background: #232129;
   color: #666360;
+  background: #232129;
+
+  ${props =>
+    props.isFocused &&
+    css`
+      border-color: #ff9000;
+      color: #ff9000;
+    `}
 
   & + div {
     margin-top: 8px;
