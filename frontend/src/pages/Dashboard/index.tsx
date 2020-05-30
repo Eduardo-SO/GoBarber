98b2b97fc2ca730ce44477ec3bc098e74144ed90
Eduardo SO <eduardo.sool07@gmail.com>
2020-05-29 22:30:15 -0300

    add: Header

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { FiPower } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+import { Container, Header, HeaderContent, Profile } from './styles';
+import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
   return (
     <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
       <h1>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab numquam
         dolore eaque eos quaerat rerum similique ullam maiores alias qui?
