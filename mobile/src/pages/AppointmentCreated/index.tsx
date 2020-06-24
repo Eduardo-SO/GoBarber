e358dbeca88eee7a46436799aebbf1d09339f217
Eduardo SO <eduardo.sool07@gmail.com>
2020-06-23 23:22:15 -0300

    add: SignOut button to new pages

--- mobile/src/pages/AppointmentCreated/index.tsx
+++ mobile/src/pages/AppointmentCreated/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Button } from 'react-native';
+
+import { useAuth } from '../../hooks/auth';
 
 import { Container } from './styles';
 
 const AppointmentCreated: React.FC = () => {
-  return <Container />;
+  const { signOut } = useAuth();
+
+  return (
+    <Container>
+      <Button title="Sair" onPress={signOut} />
+    </Container>
+  );
 };
 
 export default AppointmentCreated;
