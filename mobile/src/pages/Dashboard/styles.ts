777526dd91b633fb3dbb45f437a29f4fdb057dc3
Eduardo SO <eduardo.sool07@gmail.com>
2020-06-25 12:27:14 -0300

    add: Navigation to profile

--- mobile/src/pages/Dashboard/styles.ts
+++ mobile/src/pages/Dashboard/styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
 `;
 
 export const Header = styled.View`
@@ -11,6 +11,7 @@ export const Header = styled.View`
   align-items: center;
 
   padding: 24px;
+  padding-top: ${getStatusBarHeight() + 24}px;
   background: #28262e;
 `;
 
@@ -24,4 +25,12 @@ export const HeaderTitle = styled.Text`
 export const UserName = styled.Text`
   color: #ff9000;
   font-family: 'RobotoSlab-Medium';
+`;
+
+export const ProfileButton = styled.TouchableOpacity``;
+
+export const UserAvatar = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
 `;
