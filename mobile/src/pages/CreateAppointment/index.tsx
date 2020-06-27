0d9254d949cdce1fab0839bb1dc9eb8bf1ac935b
Eduardo SO <eduardo.sool07@gmail.com>
2020-06-26 22:25:57 -0300

    Header styles

--- mobile/src/pages/CreateAppointment/index.tsx
+++ mobile/src/pages/CreateAppointment/index.tsx
@@ -1,16 +1,41 @@
-import React from 'react';
-import { Button } from 'react-native';
+import React, { useCallback } from 'react';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import { useAuth } from '../../hooks/auth';
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+} from './styles';
 
-import { Container } from './styles';
+interface RouteParams {
+  providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
-  const { signOut } = useAuth();
+  const { user } = useAuth();
+  const route = useRoute();
+  const { goBack } = useNavigation();
+
+  const { providerId } = route.params as RouteParams;
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
 
   return (
     <Container>
-      <Button title="Sair" onPress={signOut} />
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
     </Container>
   );
 };
